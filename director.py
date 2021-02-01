a5cb010466cdf336be182527f7da93fbbcd0d802
Tomas Lazauskas <12182911+tomaslaz@users.noreply.github.com>
2021-02-01 15:02:32 +0200

    pre-commit

--- director.py
+++ director.py
@@ -1,24 +1,27 @@
-import logging
-logging.basicConfig()
-
 import pika
 import uuid
 
-class FibonacciRpcClient(object):
+import logging
 
+logging.basicConfig()
+
+
+class FibonacciRpcClient(object):
     def __init__(self):
         self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host='localhost'))
+            pika.ConnectionParameters(host="localhost")
+        )
 
         self.channel = self.connection.channel()
 
-        result = self.channel.queue_declare(queue='', exclusive=True)
+        result = self.channel.queue_declare(queue="", exclusive=True)
         self.callback_queue = result.method.queue
 
         self.channel.basic_consume(
             queue=self.callback_queue,
             on_message_callback=self.on_response,
-            auto_ack=True)
+            auto_ack=True,
+        )
 
     def on_response(self, ch, method, props, body):
         if self.corr_id == props.correlation_id:
@@ -28,13 +31,14 @@ class FibonacciRpcClient(object):
         self.response = None
         self.corr_id = str(uuid.uuid4())
         self.channel.basic_publish(
-            exchange='',
-            routing_key='rpc_queue_pelican',
+            exchange="",
+            routing_key="rpc_queue_pelican",
             properties=pika.BasicProperties(
                 reply_to=self.callback_queue,
                 correlation_id=self.corr_id,
             ),
-            body=str(n))
+            body=str(n),
+        )
         while self.response is None:
             self.connection.process_data_events()
         return int(self.response)
@@ -43,13 +47,14 @@ class FibonacciRpcClient(object):
         self.response = None
         self.corr_id = str(uuid.uuid4())
         self.channel.basic_publish(
-            exchange='',
-            routing_key='rpc_queue_panther',
+            exchange="",
+            routing_key="rpc_queue_panther",
             properties=pika.BasicProperties(
                 reply_to=self.callback_queue,
                 correlation_id=self.corr_id,
             ),
-            body=str(n))
+            body=str(n),
+        )
         while self.response is None:
             self.connection.process_data_events()
         return int(self.response)
