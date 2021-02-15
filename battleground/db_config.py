e26669a16908273b6833ad36b5d002896a016702
nbarlowATI <nbarlow@turing.ac.uk>
2021-02-15 15:58:11 +0000

    db_config.py can deal with sqlite or postgres db

--- battleground/db_config.py
+++ battleground/db_config.py
@@ -1,3 +1,11 @@
+import os
 
-
-DB_CONNECTION_STRING = "sqlite:////tmp/plarkdata.db"
+if os.environ["PlarkDBType"] == "sqlite":
+    DB_CONNECTION_STRING = "sqlite:////tmp/plarkdata.db"
+else:
+    DB_CONNECTION_STRING = "postgres://{}:{}@{}/{}".format(
+        os.environ["PlarkDBUser"],
+        os.environ["PlarkDBPassword"],
+        os.environ["PlarkDBUri"],
+        os.environ["PlarkDBName"]
+    )
