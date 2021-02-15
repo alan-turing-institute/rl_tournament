13e2015eeb0d4dc6b660838977042807676c0098
nbarlowATI <nbarlow@turing.ac.uk>
2021-02-15 18:25:15 +0000

    change naming convention for DB variables

--- battleground/db_config.py
+++ battleground/db_config.py
@@ -1,11 +1,11 @@
 import os
 
-if os.environ["PlarkDBType"] == "sqlite":
+if os.environ["DB_TYPE"] == "sqlite":
     DB_CONNECTION_STRING = "sqlite:////tmp/plarkdata.db"
 else:
     DB_CONNECTION_STRING = "postgres://{}:{}@{}/{}".format(
-        os.environ["PlarkDBUser"],
-        os.environ["PlarkDBPassword"],
-        os.environ["PlarkDBUri"],
-        os.environ["PlarkDBName"],
+        os.environ["DB_USER"],
+        os.environ["DB_PASSWORD"],
+        os.environ["DB_URI"],
+        os.environ["DB_NAME"],
     )
