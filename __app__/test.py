413fd8a0f0c9aab64f6f28d84732e3908b3deabb
Tomas Lazauskas <12182911+tomaslaz@users.noreply.github.com>
2021-02-17 19:27:16 +0200

    Tournament

--- __app__/test.py
+++ __app__/test.py
@@ -2,14 +2,20 @@
 """
 
 import os
+import subprocess
 import requests
+import logging
+from datetime import date
+import time
 
-# import subprocess
-# import create_match
+from battleground.azure_config import config as az_config
+from battleground.azure_utils import list_directory
+from battleground.db_utils import create_match, match_finished
 
-from battleground.db_utils import create_match
-
-# match_finished
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(funcName)s: %(message)s",
+)
 
 # CONST_TEAMS_LIST = ["team1", "team2", "team3", "team4", "team5", "team_test"]
 CONST_TEAMS_LIST = ["team_test"]
@@ -17,9 +23,10 @@ CONST_GITHUB_ADDRESS = (
     "https://raw.githubusercontent.com/"
     + "alan-turing-institute/rl_tournament/main/teams/"
 )
-CONST_DOCKER_COMPOSE_TEMPLATE = "docker-compose.yml_template"
+CONST_DOCKER_COMPOSE_TEMPLATE = "docker-compose-template.yml"
 CONST_TOURNAMENT_FILE = "/tmp/tournament.txt"
 CONST_TEMP_DOCKER_COMPOSE = "/tmp/docker-compose.yml"
+CONST_DEFAULT_MATCH_CONFIG_FILE = "10x10_balanced.json"
 
 
 def get_team_repository_tags(team_name):
@@ -48,16 +55,17 @@ def create_tournament():
 
     """
 
+    logging.info("Preparing for the tournament")
+
     pelicans = []
     panthers = []
 
+    logging.info("No of teams participating: %d" % len(CONST_TEAMS_LIST))
+
     for team in CONST_TEAMS_LIST:
         tags = get_team_repository_tags(team)
-
         if len(tags) > 0:
-
             for tag in tags:
-
                 if "pelican" in tag:
                     pelicans.append("%s:%s" % (team, tag))
                 elif "panther" in tag:
@@ -65,6 +73,9 @@ def create_tournament():
 
     pelicans = list(set(pelicans))
     panthers = list(set(panthers))
+
+    logging.info("No of PELICANS participating: %d" % len(pelicans))
+    logging.info("No of PANTHERS participating: %d" % len(panthers))
 
     f = open(CONST_TOURNAMENT_FILE, "w")
 
@@ -75,11 +86,62 @@ def create_tournament():
 
     f.close()
 
+    logging.info(
+        "Tournament file %s has been created." % (CONST_TOURNAMENT_FILE)
+    )
+
+
+def get_match_config_file():
+    """
+    Looks for a config file depending on the curent day. If a match file
+        for the current day cannot be found, a default will be used.
+
+    Returns:
+        a list of files in the matches container
+    """
+
+    config_file_name = None
+
+    current_day = date.today().strftime("%Y_%m_%d")
+
+    logging.info("Looking for a config file for: %s" % (current_day))
+
+    path = ""
+    container_name = az_config["config_container_name"]
+
+    config_list = list_directory(path, container_name)
+
+    if len(config_list) == 0:
+        return config_file_name
+
+    for config_file in config_list:
+        if config_file.startswith(current_day):
+            config_file_name = config_file
+            break
+
+    if config_file_name is None:
+
+        for config_file in config_list:
+            if config_file == CONST_DEFAULT_MATCH_CONFIG_FILE:
+                config_file_name = config_file
+                break
+
+    logging.info("For %s will use %s" % (current_day, config_file_name))
+
+    return config_file_name
+
 
 def run_tournament():
     """
     Runs the tournament by running multiple docker-compose files
+
+    Returns:
+        success - flag whether the tournament was executed successfully
+        error - error message, if any
     """
+
+    success = True
+    error = ""
 
     path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
@@ -91,7 +153,18 @@ def run_tournament():
     with open(CONST_TOURNAMENT_FILE, "r") as file:
         matches = file.read().splitlines()
 
-    for match in matches:
+    # get a match config file for the day
+    config_file_name = get_match_config_file()
+
+    if config_file_name is None:
+        return False, "Could not find a match config file."
+
+    no_matches = len(matches)
+    logging.info("The tournament will have %d matche(s)" % (no_matches))
+
+    for match_idx, match in enumerate(matches):
+
+        logging.info("Running match %d/%d" % (match_idx + 1, no_matches))
 
         match_yaml = template
 
@@ -99,42 +172,79 @@ def run_tournament():
 
         pelican_team, pelican_image_tag = pelican.split(":")
         panther_team, panther_image_tag = panther.split(":")
-        # create the match in the database
-        match_id = create_match(
-            pelican_team, pelican_image_tag, panther_team, panther_image_tag
-        )
 
+        # register new match
+        try:
+            # create the match in the database
+            match_id = create_match(
+                pelican_team,
+                pelican_image_tag,
+                panther_team,
+                panther_image_tag,
+                game_config=config_file_name,
+            )
+
+        except RuntimeError:
+            match_id = None
+
+        if match_id is None:
+            error += "%s - %s resulted into an error: %s" % (
+                pelican,
+                panther,
+                "cannot obtain match_id",
+            )
+            success = False
+            break
+
+        logging.info("match_id: %d" % (match_id))
+
+        # prepare yaml file
         match_yaml = match_yaml.replace("<<PELICAN>>", pelican)
         match_yaml = match_yaml.replace("<<PANTHER>>", panther)
         match_yaml = match_yaml.replace("<<MATCH_ID>>", str(match_id))
 
-        # get the right config filename from Azure storage
+        logging.info(
+            "writing docker-compose file: %s", CONST_TEMP_DOCKER_COMPOSE
+        )
+        # write docker-compose
+        with open(CONST_TEMP_DOCKER_COMPOSE, "w") as file:
+            file.write(match_yaml)
 
-        # create a match in the DB
-        # match_id = create_match(pelican_team,
-        # pelican_agent, panther_team, panther_agent, game_config)
+        cwd = os.getcwd()
 
-        # TODO make sure that the teams is in the database
+        logging.info("chdir /tmp")
+        os.chdir("/tmp")
 
-        # WRITE docker-compose
-        # with open(CONST_TEMP_DOCKER_COMPOSE, "w") as file:
-        #     file.write(match_yaml)
+        logging.info("docker-compose pull")
+        docker_st = time.time()
+        subprocess.run(["docker-compose", "pull"])
+        logging.info(
+            "docker-compose pull took %d s." % (time.time() - docker_st)
+        )
 
-        # cwd = os.getcwd()
+        docker_st = time.time()
+        logging.info("docker-compose up")
+        subprocess.Popen(["docker-compose", "up"])
 
-        # while not match_finished(match_id):
-        #     print("Checking for match finish ...")
-        #     time.sleep(5)
+        while (time.time() - docker_st) < 1800:
+            if match_finished(match_id):
+                logging.info("Match took %d s." % (time.time() - docker_st))
+                break
+            else:
+                logging.info("Match is still running. Sleep(5)")
+                time.sleep(5)
 
-        # subprocess.Popen(["docker-compose", "up"])
-
-        # WHILE not match completed (check DB for the number
-        # of games in the match and count the number of completed games)
-        #    time.sleep(1)
-
+        logging.info("docker-compose down")
+        docker_st = time.time()
         # subprocess.run(["docker-compose", "down"])
+        logging.info(
+            "docker-compose down took %d s." % (time.time() - docker_st)
+        )
 
-        # os.chdir(cwd)
+        logging.info("chdir %s", cwd)
+        os.chdir(cwd)
+
+    return success, error
 
 
 def clean_up():
@@ -150,8 +260,8 @@ if __name__ == "__main__":
 
     create_tournament()
 
-    run_tournament()
+    success, error = run_tournament()
 
     clean_up()
 
-    print("Finished")
+    print(success, error)
